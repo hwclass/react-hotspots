a72f2dc87b5ad3f177b6eea069787b429732d538
hwclass <harderworking.class@gmail.com>
2016-04-13 00:27:06 +0300

    linted App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,94 +1,90 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ProductDetail from './components/ProductDetail';
-import {imageProps} from './config';
+import { imageProps } from './config';
 
-require("./stylesheets/components/app");
+require('./stylesheets/components/app');
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      isEventSourceSupported : window.EventSource,
-      moduleId : 'editorial_hotspots_590359239320250140028',
-      moduleData : null,
-      selectedImage : null,
-      selectedPointer : null
-    }
+      isEventSourceSupported: window.EventSource,
+      moduleId: 'editorial_hotspots_590359239320250140028',
+      moduleData: null,
+      selectedImage: null,
+      selectedPointer: null,
+    };
     this.activateServerSentEvents();
   }
 
-  componentWillMount () {
+  componentWillMount() {
     this.state = {
-      isEventSourceSupported : window.EventSource,
-      moduleId : 'editorial_hotspots_590359239320250140028',
-      moduleData : null,
-      selectedImage : null,
-      selectedPointer : null
-    }
+      isEventSourceSupported: window.EventSource,
+      moduleId: 'editorial_hotspots_590359239320250140028',
+      moduleData: null,
+      selectedImage: null,
+      selectedPointer: null,
+    };
   }
 
-  activateServerSentEvents () {
-    if (!!this.state.isEventSourceSupported) {
-      const eventSource = new EventSource('http://localhost:3000/module/' + this.state.moduleId);
-      const self = this;
-      // eventSource.addEventListener('message', (response) => {
-      //   this.state
-      //   const moduleData = JSON.parse(response.data);
-      //   self.setState({
-      //     moduleData
-      //   })
-      // }, false);
-    } else {
-      console.log('An error occured while fetching data.');
-    }
-  }
-
-  onImagePointerClick (selectedPointer) {
+  onImagePointerClick(selectedPointer) {
     this.setState({
       selectedPointer,
-      selectedImage : selectedPointer 
-    })
+      selectedImage: selectedPointer,
+    });
   }
 
-  render () {
+  activateServerSentEvents() {
+    if (!!this.state.isEventSourceSupported) {
+      const sseUrl = `http://localhost:3000/module/$ + ${this.state.moduleId}`;
+      const eventSource = new EventSource(sseUrl);
+      const self = this;
+      eventSource.addEventListener('message', (response) => {
+        self.setState({
+          moduleData: JSON.parse(response.data),
+        });
+      }, false);
+    }
+  }
 
+  render() {
     const images = [
       {
         src: 'http://sonyglobal.scene7.com/is/image/gwtprod/93a8c28123eede1d42d33871e6553daf',
-        hotspots : [
+        hotspots: [
           {
             top: '10%',
             left: '20%',
-            tooltip : {
-              title : 'Modal 1 Title',
-              text : 'Some tooltip content...'
-            }
-          }
-        ]
+            tooltip: {
+              title: 'Modal 1 Title',
+              text: 'Some tooltip content...',
+            },
+          },
+        ],
       },
       {
         src: 'http://sonyglobal.scene7.com/is/image/gwtprod/54c77aef97fe56422f0e6c9ba105b23a',
-        hotspots : [
+        hotspots: [
           {
             top: '1%',
             left: '1%',
-            tooltip : {
-              title : 'Modal 2 Title',
-              text : 'Some tooltip content...'
-            }
+            tooltip: {
+              title: 'Modal 2 Title',
+              text: 'Some tooltip content...',
+            },
           },
           {
             top: '5%',
             left: '5%',
-            tooltip : {
-              title : 'Modal 3 Title',
-              text : 'Some tooltip content...'
-            }
-          }
-        ]
-      }
+            tooltip: {
+              title: 'Modal 3 Title',
+              text: 'Some tooltip content...',
+            },
+          },
+        ],
+      },
     ];
 
     return (
@@ -98,12 +94,11 @@ class App extends Component {
           moduleData={this.state.moduleData}
           images={images}
           selectedImage={(this.state.selectedImage === null ? images[0] : this.state.selectedImage)}
-          onImagePointerClick={selectedPointer => this.onImagePointerClick(selectedPointer)}/>
-      </div>
+          onImagePointerClick={selectedPointer => this.onImagePointerClick(selectedPointer)} 
+      />
+      </div>;
     )
-
   }
-
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
