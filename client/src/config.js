62d8c3d36cce952d7d6b1245d1f84c627568e0d3
hwclass <harderworking.class@gmail.com>
2016-04-17 00:27:22 +0300

    fixed eslint issues

--- client/src/config.js
+++ client/src/config.js
@@ -1,31 +1,31 @@
-const imageFormat = 'jpeg'
+const imageFormat = 'jpeg';
 
 const sizes = {
-	desktop : 1440,
-	tablet : 720,
-	phone : 480,
-	highRes : {
-		desktop : 2880,
-		tablet : 1440,
-		phone : 960
-	}
+  desktop: 1440,
+  tablet: 720,
+  phone: 480,
+  highRes: {
+    desktop: 2880,
+    tablet: 1440,
+    phone: 960,
+  },
 };
 
 const imageProps = {
-  queries : {
-    desktop : '?fmt=' + imageFormat + '&wid=' + sizes.desktop,
-    tablet : '?fmt=' + imageFormat + '&wid=' + sizes.tablet,
-    phone : '?fmt=' + imageFormat + '&wid=' + sizes.phone,
-    highRes : {
-    	desktop : '?fmt=' + imageFormat + '&wid=' + sizes.highRes.desktop,
-    	tablet : '?fmt=' + imageFormat + '&wid=' + sizes.highRes.tablet,
-    	phone : '?fmt=' + imageFormat + '&wid=' + sizes.highRes.phone,
-    }
-  }
+  queries: {
+    desktop: '?fmt=${imageFormat}' + '&wid=${sizes.desktop}',
+    tablet: '?fmt=${imageFormat}' + '&wid=${sizes.tablet}',
+    phone: '?fmt=${imageFormat}' + '&wid=${sizes.phone}',
+    highRes: {
+      desktop: '?fmt=${imageFormat}' + '&wid=${sizes.highRes.desktop}',
+      tablet: '?fmt=${imageFormat}' + '&wid=${sizes.highRes.tablet}',
+      phone: '?fmt=${imageFormat}' + '&wid=${sizes.highRes.phone}',
+    },
+  },
 };
 
 export {
 	imageFormat,
 	sizes,
-	imageProps
+	imageProps,
 };
